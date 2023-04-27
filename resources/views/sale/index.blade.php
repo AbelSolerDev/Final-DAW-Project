f86fbf63e006b3582923cbca074a6e9e453cd2b6
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2023-04-27 18:30:22 +0200

    Reviewed and corrected Sale view applying Sale table

--- resources/views/sale/index.blade.php
+++ resources/views/sale/index.blade.php
@@ -12,11 +12,60 @@
         <p>{{ $description }}</p>
         <p>{{ $description2 }}</p>
     </div>
+
     <div class="row publicaciónMobilHome">
         @php
             $saleIds = \App\Models\Sale::pluck('mobil_home_id')->toArray();
         @endphp
-    <!-- MOBILHOME SIN VENDER -->
+    <!-- MOBILHOME SIN VENDER / SIN DESCUENTO / SIN HACER LOGIN-->
+        @foreach ($mobilHomes->items() as $mobilHome)
+            @php
+                $isOnSale = in_array($mobilHome->id, $saleIds);
+            @endphp
+            @if (!$isOnSale && !Auth::check() && $mobilHome->discounted_price < 1)
+            <div class="col-md-4 col-lg-3 mb-4 mx-auto">
+                <div class="card h-100">
+                    @if ($mobilHome->images->count() > 0)
+                    <img src="{{ $mobilHome->images->first() ? '/storage/' . $mobilHome->images->first()->image_path : '/storage/mobilhome_images/default.jpg' }}" alt="Photo of {{ $mobilHome->name }}" class="img-fluid thumbnail-img">
+                    @else
+                        No Images
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $mobilHome->name }}</h5>
+                        <hr>
+                        <p class="card-text">{{ Str::limit($mobilHome->description, $limit = 100, $end = '...') }}</p>
+                        <hr>
+                        <p class="card-text text-right">
+                            <span>{{ $mobilHome->price }}&euro;</span>
+                        </p>
+                        <p>
+                            @if ($mobilHome->available)
+                                <span class="text-success font-weight-bold">Available!</span>
+                            @endif
+                        </p>
+                    </div>
+                    @if (auth()->check() && auth()->user()->is_admin)
+                        <a 
+                            href="{{ route('sale.show', $mobilHome) }}" 
+                            class="btn btn-special {{ $mobilHome->on_sale ? 'enable' : '' }}" 
+                            style="{{ $mobilHome->on_sale ? 'background-color: #ccc; color: #000;' : '' }}">
+                                Know More
+                        </a>
+                    @else
+                    <a 
+                        href="{{ route('sale.show', $mobilHome) }}" 
+                        class="btn btn-special {{ $mobilHome->on_sale ? 'disabled' : '' }}" 
+                        style="{{ $mobilHome->on_sale ? 'background-color: #ccc; color: #000;' : '' }}">
+                            Know More
+                    </a>
+                    @endif
+                </div>
+            </div>
+            @endif
+        @endforeach
+
+           <!-- MOBILHOME SIN VENDER / CON DESCUENTO / CON LOGIN-->
+    @if (Auth::check())
         @foreach ($mobilHomes->items() as $mobilHome)
             @php
                 $isOnSale = in_array($mobilHome->id, $saleIds);
@@ -78,6 +127,7 @@
             </div>
             @endif
         @endforeach
+
     <!-- MOBILHOME VENDIDOS -->
         @foreach ($mobilHomes->items() as $mobilHome)
             @php
@@ -140,6 +190,7 @@
             </div>
             @endif
         @endforeach
+    @endif
 
         <div class="d-flex justify-content-center">
         {{ $mobilHomes->onEachSide(3)->links() }}
