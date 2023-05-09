e7d8706a838d7e3f2ce7285ce0f718f4edf5b97f
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2023-05-09 14:32:43 +0200

    reset without github pages

--- vite.config.js
+++ vite.config.js
@@ -8,24 +8,24 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    base: "/proyecto-final/",
 });
 
 /*
-Estado Previo:
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+En el archivo vite.config.js, agregue la configuración de base para que la aplicación 
+se ejecute en la carpeta /proyecto-final/ en lugar de la raíz del servidor web. 
+El archivo vite.config.js se verá así:
+
+
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    base: "/proyecto-final/",
 });
-
 */
