f16dfa6120428d40eaa707baab0d1b4c20e1da97
Abel Soler <102069966+AbelSolerDev@users.noreply.github.com>
2023-05-09 13:17:31 +0200

    github pages active

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,18 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            refresh: true,
+        }),
+    ],
+    base: "/proyecto-final/",
+});
+
+/*
+Estado Previo:
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -12,3 +27,5 @@ export default defineConfig({
         }),
     ],
 });
+
+*/
